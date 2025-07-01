4c28480b23940156c4c7c6b7b53a132dd8559f48
Piyush Dahiwadkar <97776547+Piyush2627@users.noreply.github.com>
2025-07-01 15:52:32 +0530

    Attendance

--- client/src/pages/AttendanceDashboard.tsx
+++ client/src/pages/AttendanceDashboard.tsx
@@ -1,24 +1,14 @@
-import AddAttendanceForm from "./AddAttendanceForm";
+// import AddAttendanceForm from "./AddAttendanceForm";
 import AttendanceList from "./AttendanceList";
 
 function AttendanceDashboard() {
   return (
-    <div className="container mx-auto flex space-x-8 px-4 py-6">
-      {/* Form at the top */}
-      <div className="mb-8">
-        <h2 className="mb-4 text-2xl font-semibold">Mark Attendance</h2>
-        <div className="max-w-xl">
-          <AddAttendanceForm />
-        </div>
-      </div>
+    <div className="container mx-auto px-4 py-6">
+      <h1 className="mb-6 text-3xl font-bold text-gray-800">
+        Attendance Dashboard
+      </h1>
 
-      {/* Attendance list below */}
-      <div>
-        <h2 className="mb-4 text-2xl font-semibold">Attendance Records</h2>
-        <div className="h-screen w-full overflow-auto rounded-xl bg-white p-6 pr-2 shadow-lg sm:p-8">
-          <AttendanceList />
-        </div>
-      </div>
+      <AttendanceList />
     </div>
   );
 }
